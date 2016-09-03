97c851efc1bffc95aa51207822f519d0051cc573
Rav3nPL <rav3n.pl@gmail.com>
2016-09-03 09:29:08 +0200

    Gather data for DGW before hardfork
    to prevent diff=1 when DGW kicks in

--- src/main.cpp
+++ src/main.cpp
@@ -1476,6 +1476,11 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     }
     if (pindexLast->nHeight < 1000) 
 	{
+		if (pindexLast->nHeight < 975)
+		{
+			printf("Gathering data for DGW...");
+			DarkGravityWave3(pindexLast, pblock);
+		}
         return GetNextWorkRequired_v1(pindexLast, pblock);
 	}
     return DarkGravityWave3(pindexLast, pblock);
