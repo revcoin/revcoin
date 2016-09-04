5e293ed09d3e59418f924cee2867e9cc1d2231d0
rav3n_pl <rav3n.pl@gmail.com>
2016-09-04 16:04:23 +0200

    Revert "fix "Ambiguous" errors" this is for qt5 only - another branch
    This reverts commit f6df888c256b6d071da3d2add0aa2992954c1158.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -107,11 +107,11 @@ public:
 	vSeeds.push_back(CDNSSeedData("revcoin.org", "89.248.170.169"));
 
 
-        base58Prefixes[PUBKEY_ADDRESS] = {60};
-        base58Prefixes[SCRIPT_ADDRESS] = {5};
-        base58Prefixes[SECRET_KEY] =     {188};
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(60);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(5);
+        base58Prefixes[SECRET_KEY] =     list_of(188);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4);
 
         // Convert the pnSeeds array into usable address objects.
         for (unsigned int i = 0; i < ARRAYLEN(pnSeed); i++)
@@ -169,11 +169,11 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
 
-        base58Prefixes[PUBKEY_ADDRESS] = {111};
-        base58Prefixes[SCRIPT_ADDRESS] = {196};
-        base58Prefixes[SECRET_KEY]     = {239};
-        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
-        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
+        base58Prefixes[PUBKEY_ADDRESS] = list_of(111);
+        base58Prefixes[SCRIPT_ADDRESS] = list_of(196);
+        base58Prefixes[SECRET_KEY]     = list_of(239);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF);
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94);
     }
     virtual Network NetworkID() const { return CChainParams::TESTNET; }
 };
